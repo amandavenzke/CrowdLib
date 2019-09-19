2b68a4e05b243d766ac6a60a51e9198911f82b54
Venzke, Amanda <Amanda.Venzke@br.experian.com>
2019-09-19 20:30:50 -0300

    Configuracoes iniciais para nuvem

--- src/main/java/com/crowdlib/api/storage/S3.java
+++ src/main/java/com/crowdlib/api/storage/S3.java
@@ -1,5 +1,14 @@
 package com.crowdlib.api.storage;
 
-public class S3 {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.s3.AmazonS3;
+
+@Component
+public class S3 {
+	
+	@Autowired
+	private AmazonS3 amazonS3;
+	
 }
