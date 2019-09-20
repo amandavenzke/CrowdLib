b3ba95113b2308243474b136e2540a04adb8e899
Venzke, Amanda <Amanda.Venzke@br.experian.com>
2019-09-20 16:38:55 -0300

    Alteracao para nuvem

--- src/main/resources/db/migration/V01__criar_usuario_livro_permissao.sql
+++ src/main/resources/db/migration/V01__criar_usuario_livro_permissao.sql
@@ -41,8 +41,3 @@ CREATE TABLE usuario_permissao (
 	FOREIGN KEY (id_usuario) REFERENCES usuario(id),
 	FOREIGN KEY (id_permissao) REFERENCES permissao(id)
 );
-
-INSERT INTO crowdlib.usuario (nome, sexo, data_nascimento, email, senha, logradouro, complemento, numero, bairro, cidade, estado, cep, celular) VALUES ('Amanda Venzke', 'FEMININO', '1991-05-30', 'patriciavenzke@gmail.com', '$2a$10$u.WjRhyOAsgnvYWUFA86W.ALODHF7u4IauJOO75ygF0RLm6IFliVy', 'Rua Jau', 'Bloco C Apto 71', '51', 'Santo Antonio',	'Osasco', 'SP',	'6126320','551198090987');
-
-INSERT INTO crowdlib.PERMISSAO (descricao) VALUES ('ROLE_CADASTRAR_USUARIO');
-INSERT INTO crowdlib.usuario_permissao (id_usuario, id_permissao) VALUES (1, 1);
